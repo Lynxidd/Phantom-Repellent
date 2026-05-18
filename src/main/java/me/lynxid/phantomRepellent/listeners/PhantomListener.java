27b690b08ad0e228d7347f0c1c4321f3538486e0
Lynxid <TheTrueSilo@gmail.com>
2026-05-18 04:31:30 -0400

    1.0.3 - Bug Fix Attempt to fix the error where target equals null on execution of PhantomListener. This should work!
    ```- [Thu 17:32:01 ERROR Minecraft] Could not pass event EntityTargetLivingEntityEvent to Phantom-Repellent v1.0.2-Paper_Release
       java.lang.NullPointerException: Cannot invoke "org.bukkit.entity.Entity.getPersistentDataContainer()" because "target" is null
           at phantom-repellent-1.0.2-Paper_Release.jar//me.lynxid.phantomRepellent.listeners.PhantomListener.onEntityTarget(PhantomListener.java:27)```

--- src/main/java/me/lynxid/phantomRepellent/listeners/PhantomListener.java
+++ src/main/java/me/lynxid/phantomRepellent/listeners/PhantomListener.java
@@ -10,11 +10,14 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityTargetEvent;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Objects;
 
 public class PhantomListener implements Listener {
 
+    private static final Logger log = LoggerFactory.getLogger(PhantomListener.class);
     private final PhantomRepellent plugin;
     public PhantomListener(PhantomRepellent plugin)  {this.plugin = plugin;}
 
@@ -22,10 +25,11 @@ public class PhantomListener implements Listener {
     public void onEntityTarget(EntityTargetEvent e){
        if (e.getEntity().getType() != EntityType.PHANTOM) {return;}
        final Entity target = e.getTarget();
-        NamespacedKey phantomKey = new NamespacedKey(this.plugin, "phantom");
-        assert target != null;
-        PersistentDataContainer pdc = target.getPersistentDataContainer();
-        boolean phantomsOff = Objects.equals(pdc.get(phantomKey, PersistentDataType.BOOLEAN), true);
-        e.setCancelled(target instanceof Player && phantomsOff);
+       if (target != null) {
+           NamespacedKey phantomKey = new NamespacedKey(this.plugin, "phantom");
+           PersistentDataContainer pdc = target.getPersistentDataContainer();
+           boolean phantomsOff = Objects.equals(pdc.get(phantomKey, PersistentDataType.BOOLEAN), true);
+           e.setCancelled(target instanceof Player && phantomsOff);
+       }
     }
 }
